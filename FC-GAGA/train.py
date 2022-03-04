a97a4d0c59be4309e6653143e89e423582baef64
Vy Vo <52537680+isVy08@users.noreply.github.com>
2022-03-04 13:04:27 +1100

    Update

--- FC-GAGA/train.py
+++ FC-GAGA/train.py
@@ -1,3 +1,4 @@
+import sys
 import tensorflow as tf
 import numpy as np
 print("Tensorflow version:", tf.__version__)
@@ -42,60 +43,67 @@ print("*********************************")
 
 
 trainer = Trainer(hyperparams=hyperparams, logdir=LOGDIR)
-trainer.fit(dataset=dataset)
 
-for i in range(len(trainer.models)):
-    # Save models
-    model = trainer.models[i].model
-    model.save_weights(f'model/fc-gaga-{i}.hdf5')
+if sys.argv[1] == 'train':
+    trainer.fit(dataset=dataset)
+
+    for i in range(len(trainer.models)):
+        # Save models
+        model = trainer.models[i].model
+        model.save_weights(f'model/fc-gaga-{i}.hdf5')
 
 
-print("*********************************")
-print("COMPUTING METRICS")
-print("*********************************")
+    print("*********************************")
+    print("COMPUTING METRICS")
+    print("*********************************")
 
-early_stop_mae_h_repeats = dict()
-early_stop_mape_h_repeats = dict()
-early_stop_rmse_h_repeats = dict()
-early_stop_mae_h_ave = dict()
-early_stop_mape_h_ave = dict()
-early_stop_rmse_h_ave = dict()
-for i, h in enumerate(trainer.history):
-    early_stop_idx = np.argmin(h['mae_val'])
-    early_stop_mae = np.round(h['mae_test'][early_stop_idx], decimals=3)
-    print(f"Early stop test error model {trainer.folder_names[i]}:", "Avg MAE", early_stop_mae)
-    for horizon in range(3, hyperparams.horizon+1, 3):
+    early_stop_mae_h_repeats = dict()
+    early_stop_mape_h_repeats = dict()
+    early_stop_rmse_h_repeats = dict()
+    early_stop_mae_h_ave = dict()
+    early_stop_mape_h_ave = dict()
+    early_stop_rmse_h_ave = dict()
+    for i, h in enumerate(trainer.history):
+        early_stop_idx = np.argmin(h['mae_val'])
+        early_stop_mae = np.round(h['mae_test'][early_stop_idx], decimals=3)
+        print(f"Early stop test error model {trainer.folder_names[i]}:", "Avg MAE", early_stop_mae)
+        for horizon in range(3, hyperparams.horizon+1, 3):
 
-        early_stop_mae_h_repeats = insert_dict(early_stop_mae_h_repeats, k=horizon, v=h[f'mae_test_h{horizon}'][early_stop_idx])
-        early_stop_mape_h_repeats = insert_dict(early_stop_mape_h_repeats, k=horizon, v=h[f'mape_test_h{horizon}'][early_stop_idx])
-        early_stop_rmse_h_repeats = insert_dict(early_stop_rmse_h_repeats, k=horizon, v=h[f'rmse_test_h{horizon}'][early_stop_idx])
-        
-        print(f"Horizon {horizon} MAE:", np.round(early_stop_mae_h_repeats[horizon][-1], decimals=2), 
-              f"Horizon {horizon} MAPE:", np.round(early_stop_mape_h_repeats[horizon][-1], decimals=2), 
-              f"Horizon {horizon} RMSE:", np.round(early_stop_rmse_h_repeats[horizon][-1], decimals=2))
+            early_stop_mae_h_repeats = insert_dict(early_stop_mae_h_repeats, k=horizon, v=h[f'mae_test_h{horizon}'][early_stop_idx])
+            early_stop_mape_h_repeats = insert_dict(early_stop_mape_h_repeats, k=horizon, v=h[f'mape_test_h{horizon}'][early_stop_idx])
+            early_stop_rmse_h_repeats = insert_dict(early_stop_rmse_h_repeats, k=horizon, v=h[f'rmse_test_h{horizon}'][early_stop_idx])
+            
+            print(f"Horizon {horizon} MAE:", np.round(early_stop_mae_h_repeats[horizon][-1], decimals=2), 
+                f"Horizon {horizon} MAPE:", np.round(early_stop_mape_h_repeats[horizon][-1], decimals=2), 
+                f"Horizon {horizon} RMSE:", np.round(early_stop_rmse_h_repeats[horizon][-1], decimals=2))
 
-    for horizon in range(3, hyperparams.horizon+1, 3):
-        early_stop_mae_h_ave[horizon] = np.round(np.mean(early_stop_mae_h_repeats[horizon]), decimals=2)
-        early_stop_mape_h_ave[horizon] = np.round(np.mean(early_stop_mape_h_repeats[horizon]), decimals=2)
-        early_stop_rmse_h_ave[horizon] = np.round(np.mean(early_stop_rmse_h_repeats[horizon]), decimals=2)
+        for horizon in range(3, hyperparams.horizon+1, 3):
+            early_stop_mae_h_ave[horizon] = np.round(np.mean(early_stop_mae_h_repeats[horizon]), decimals=2)
+            early_stop_mape_h_ave[horizon] = np.round(np.mean(early_stop_mape_h_repeats[horizon]), decimals=2)
+            early_stop_rmse_h_ave[horizon] = np.round(np.mean(early_stop_rmse_h_repeats[horizon]), decimals=2)
 
-print()
-print("Average MAE:", early_stop_mae_h_ave)
-print("Average MAPE:", early_stop_mape_h_ave)
-print("Average RMSE:", early_stop_rmse_h_ave)
+    print()
+    print("Average MAE:", early_stop_mae_h_ave)
+    print("Average MAPE:", early_stop_mape_h_ave)
+    print("Average RMSE:", early_stop_rmse_h_ave)
 
 
 # FULL PREDICTIONS
 
-# from utils import MetricsCallback
-# metrics = MetricsCallback(dataset=dataset, logdir=LOGDIR)
-# predictions = best_model.predict({"history": metrics.full_data["x"][...,0], 
-#                                     "node_id": metrics.full_data["node_id"],
-#                                     "time_of_day": metrics.full_data["x"][...,0]})
-# np.savez_compressed(
-#     os.path.join(DATADIR + '/stvar/full_predictions.npz'),
-#     input=metrics.full_data["x"],
-#     truth=metrics.full_data["y"],
-#     prediction=predictions['targets']
-    
-#     )
+elif sys.argv[1] == 'val':
+
+    from utils import MetricsCallback
+    path = f'model/fc-gaga-{sys.argv[2]}.hdf5'
+    metrics = MetricsCallback(dataset=dataset, logdir=LOGDIR)
+    best_model = trainer.models[-1].model
+    best_model.load_weights()
+    predictions = best_model.predict({"history": metrics.full_data["x"][...,0], 
+                                        "node_id": metrics.full_data["node_id"],
+                                        "time_of_day": metrics.full_data["x"][...,0]})
+    np.savez_compressed(
+        os.path.join(DATADIR + '/stvar/full_predictions.npz'),
+        input=metrics.full_data["x"],
+        truth=metrics.full_data["y"],
+        prediction=predictions['targets']
+        
+        )
