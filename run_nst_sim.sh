f491d3c117f6973977d8e6e14bee5a50cf14fc59
Vy Vo <52537680+isVy08@users.noreply.github.com>
2022-04-19 00:24:59 +1000

    Update

--- run_nst_sim.sh
+++ run_nst_sim.sh
@@ -1,5 +1,5 @@
 for i in {0..99}
 do
     # python nst_simulation.py data/nst_sim/csv/s$i.csv output/nst_sim/out$i.pickle model/nst_sim/model$i.pt
-    python nst_simulation.py data/new_sim/csv/s$i.csv output/new_sim/out$i.pickle model/new_sim/model$i.pt
+    python nst.py data/quad_sim/csv/s$i.csv output/quad_sim/out$i.pickle model/quad_sim/model$i.pt $i
 done
--- utils.py
+++ utils.py
@@ -1,24 +1,6 @@
-import json, pickle
+import pickle
 import numpy as np
 import torch
-
-class Namespace:
-  def __init__(self, **kwargs):
-    self.__dict__.update(kwargs)
-
-def get_config(config_file): 
-  with open(config_file) as f:
-        config = json.load(f)
-  n = Namespace()
-  n.__dict__.update(config)
-  return n
-
-def standardize(ts, dim, means=None, stds=None):
-  if means is None:
-    means = ts.mean(dim=dim, keepdim=True)
-  if stds is None:
-    stds = ts.std(dim=dim, keepdim=True)
-  return (ts - means) / stds
 
 def load(datadir):
     with open(datadir, encoding='utf-8') as f:
